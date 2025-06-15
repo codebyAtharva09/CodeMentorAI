23b9912a33b930e80c6b3d4302d175145a347aae
Atharva Sarde <atharva.sarde0903@gmail.com>
2025-06-15 09:30:12 +0530

    Update api.js

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://9302fa41-141f-4af5-8595-e5d98fcd6be5-00-1gg5uxwhu975n.sisko.replit.dev/";
+const BASE_URL = "https://codementorai-71fu.onrender.com";
 
 export const loginUser = async (username, password) => {
   return username === "testuser" && password === "password123";
